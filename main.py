9c3857dcfb8d1c1b08047f1e512b8a949730ed54
Jovia A J <jovia.aj2024aiml@sece.ac.in>
2026-07-29 00:40:55 +0530

    Removed import of non-existent missing_module to resolve ModuleNotFoundError

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # incident-test-app/main.py
-# INTENTIONAL BUG: missing_module import will fail
-import missing_module  # This module does not exist -> ImportError
+# INTENTIONAL BUG: Removed to prevent import error
 
 def compute(a, b):
     return a / b  # Also: ZeroDivisionError if b=0
