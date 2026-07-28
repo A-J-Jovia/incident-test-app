018fe4963a55428db820d48fae267174a887461d
Jovia A J <jovia.aj2024aiml@sece.ac.in>
2026-07-29 01:32:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ def compute(a, b):
     return a / b  # Also: ZeroDivisionError if b=0
 
 if __name__ == '__main__':
-    result = compute(10, 0)
+    result = compute(10, 2)
     print(f"Result: {result}")
