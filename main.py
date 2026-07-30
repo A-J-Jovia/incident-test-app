494947f8f9b556318d6e870f0001fe01995a804b
Jovia A J <jovia.aj2024aiml@sece.ac.in>
2026-07-30 08:49:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import non_existent_webhook_test_module
 import absolutely_broken_module_test
 import missing_module_for_ci_test
-import wrng 
+import gddtd 
 
 # incident-test-app/main.py
 # INTENTIONAL BUG: missing_module import will fail
