2fb76dcb5239e6c90296d7f7ab8eeb7879bc31c0
Jovia A J <jovia.aj2024aiml@sece.ac.in>
2026-07-30 07:56:10 +0530

    Fix ModuleNotFoundError by removing non-existent imports

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import non_existent_webhook_test_module
-import absolutely_broken_module_test
-import missing_module_for_ci_test
 import wrng 
 
 # incident-test-app/main.py
 # INTENTIONAL BUG: missing_module import will fail
-import missing_module  # This module does not exist -> ImportError
+# import missing_module  # This module does not exist -> ImportError
+# import non_existent_webhook_test_module
+# import absolutely_broken_module_test
+# import missing_module_for_ci_test
 
 def compute(a, b):
     return a / b  # Also: ZeroDivisionError if b=0
